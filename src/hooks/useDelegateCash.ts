1e0001f28ab8dd86fb0cf18e9d6250c2212a96bc
cawfree <hello@cawfree.com>
2023-01-30 15:22:58 +0000

    chore: recreate on delta signer

--- src/hooks/useDelegateCash.ts
+++ src/hooks/useDelegateCash.ts
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import {DelegateCash} from 'delegatecash';
 import {ethers} from 'ethers';
-import {useClient} from 'wagmi';
+import {useClient, useSigner} from 'wagmi';
+
+type DelegateCashProvider = ethers.providers.Web3Provider | ethers.providers.JsonRpcProvider;
 
 export function useDelegateCash() {
-  const {provider} = useClient<
-    ethers.providers.Web3Provider | ethers.providers.JsonRpcProvider
-  >();
+  const {provider: defaultProvider} = useClient<DelegateCashProvider>();
+
+  const {data: signer} = useSigner();
+
+  const provider = React.useMemo<DelegateCashProvider>(() => ({
+    ...defaultProvider,
+    // @ts-ignore
+    //getSigner: () => signer,
+  }), [defaultProvider, signer]);
+
   return React.useMemo(
     () => new DelegateCash(provider),
     [provider]
