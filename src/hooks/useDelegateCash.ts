c9278b257fa0374f8edc7e2dc9c7cfb4dac1952c
cawfree <hello@cawfree.com>
2023-01-30 16:17:47 +0000

    chore: async assign signer

--- src/hooks/useDelegateCash.ts
+++ src/hooks/useDelegateCash.ts
@@ -5,12 +5,30 @@ import {useProvider, useSigner} from 'wagmi';
 
 type DelegateCashProvider = ethers.providers.Web3Provider | ethers.providers.JsonRpcProvider;
 
+const instantiateDelegateCash = (
+  provider: DelegateCashProvider | undefined
+) => {
+  try {
+    if (provider) return new DelegateCash(provider);
+
+    return new DelegateCash();
+  } catch (e) {
+    return new DelegateCash();
+  }
+};
+
 export function useDelegateCash() {
   const provider = useProvider<DelegateCashProvider>();
   const {data: signer} = useSigner();
 
   return React.useMemo(
-    () => new DelegateCash(provider),
+    () => {
+      const dc = instantiateDelegateCash(provider);
+
+      if (signer) dc.signer = signer;
+
+      return dc;
+    },
     [provider, signer]
   );
 }
