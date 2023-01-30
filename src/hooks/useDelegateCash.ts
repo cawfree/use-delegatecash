44d48dbc57863c2bbacf5fd4203a0079c4c055f5
cawfree <hello@cawfree.com>
2023-01-30 15:45:14 +0000

    fix: realloc on signer

--- src/hooks/useDelegateCash.ts
+++ src/hooks/useDelegateCash.ts
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import {DelegateCash} from 'delegatecash';
 import {ethers} from 'ethers';
-import {useProvider} from 'wagmi';
+import {useProvider, useSigner} from 'wagmi';
 
 type DelegateCashProvider = ethers.providers.Web3Provider | ethers.providers.JsonRpcProvider;
 
 export function useDelegateCash() {
   const provider = useProvider<DelegateCashProvider>();
+  const {data: signer} = useSigner();
+
   return React.useMemo(
     () => new DelegateCash(provider),
-    [provider]
+    [provider, signer]
   );
 }
